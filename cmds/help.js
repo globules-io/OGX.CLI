14d15adbe6f47a5f493073d44622d079e1beb563
Eric <info@ericparlier.com>
2020-05-05 19:21:38 -0400

    Added link and unlink

--- cmds/help.js
+++ cmds/help.js
@@ -39,6 +39,16 @@ const menus = {
         syntax      : ogx templates
         description : list and adds for preload all html templates from /www/html
 
+    link
+        syntax      : ogx link [type]
+        description : list all files of a type and links them to the index file
+        example     : ogx link all
+
+    unlink
+        syntax      : ogx link [type]
+        description : list all files of a type and unlinks them from the index file
+        example     : ogx link all
+
     build
         syntax      : ogx build [target] [optional mode]
         description : build the application
@@ -62,7 +72,15 @@ const menus = {
     delete: `
     syntax : ogx create [type] [id] ogx   
     type   : type of object (view, controller, template or stage)
-    id     : id of object`
+    id     : id of object`,
+
+    link: `
+    syntax : ogx link [type] 
+    type   : type of object (view, controller or stage)`,
+
+    unlink: `
+    syntax : ogx unlink [type] 
+    type   : type of object (view, controller or stage)`
 };
 
 module.exports = (args) => {   
---
+++ cmds/link.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+module.exports = (args) => {
+    if(!args || !args.length){
+        console.log('Error: Missing arguments! Expected [type]');
+        return;
+    }
+    if(!/(all|views|templates|stages)/gi.test(args[0])){
+        console.log('Error: Invalid object!');
+        return;
+    }    
+    if(args.length){
+        const fs = require('fs');
+        let index = fs.readFileSync('www/index.html', 'utf-8');
+        if(index){
+            if(['all', 'views'].indexOf(args[0] !== -1)){               
+                fs.readdirSync('www/js/views').forEach(file => {                  
+                    index = index.replace('</head>', '<script type="application/javascript" src="js/views/'+file+'"></script>\n</head>');  
+                    index = index.replace('</head>', '<link rel="stylesheet" href="css/views/view.'+file.split('.')[1]+'.css">\n</head>'); 
+                    console.log('Info: linked', file);
+                });
+            } 
+            if(['all', 'templates'].indexOf(args[0] !== -1)){     
+                fs.readdirSync('www/html').forEach(file => {                  
+                    index = index.replace('</head>', '<script type="application/javascript" src="html/'+file+'"></script>\n</head>');     
+                    console.log('Info: linked', file);            
+                });  
+               
+            }
+            if(['all', 'stages'].indexOf(args[0] !== -1)){   
+                fs.readdirSync('www/js/stages').forEach(file => {                  
+                    index = index.replace('</head>', '<script type="application/javascript" src="js/stages/'+file+'"></script>\n</head>');  
+                    index = index.replace('</head>', '<link rel="stylesheet" href="css/stages/stage.'+file.split('.')[1]+'.css">\n</head>'); 
+                    console.log('Info: linked', file);
+                });       
+            }   
+            fs.writeFileSync('www/index.html', index);    
+        }else{
+            console.log('Warning: index.html file not found, linking skipped');
+        } 
+    }
+};
