3b6e01afdd5f777f2fef8bd65e64c6b998f6f30b
Eric <info@ericparlier.com>
2020-05-10 15:27:14 -0400

    Added support to themes fonts & img preload

--- cmds/fonts.js
+++ cmds/fonts.js
@@ -4,7 +4,7 @@ expected [name]-[type].[ext], ie roboto-regular.tff
 */
 module.exports = (args) => {
     const fs = require('fs');
-    const path = 'www/fonts';
+    const paths = ['/fonts'];
     let files = [];
     const reg = /(\.ttf|\.eot|\.woff|\.woff2)$/gi;
     let output = '';
@@ -18,42 +18,58 @@ module.exports = (args) => {
                 delete config.preload['/fonts'];
             }                  
         } 
-        if(fs.existsSync(path)){         
-            fs.readdirSync(path).forEach(folder => {  
-                fs.readdirSync(path+'/'+folder).forEach(file => {
-                    if(reg.test(file)){
-                        font = file.split('.')[0].split('=');
-                        output += '@font-face {\n';
-                        output += '    font-family:'+font[0]+';\n';
-                        output += '    src:url("../../fonts/'+folder+'/'+file+'");\n';
-                        output += '}\n';
-                        files.push(file);
-                        console.log('Info: added to preload', file);
-                        reg.lastIndex = 0;
-                    }
-                });            
+        //merge with themes
+        if(fs.existsSync('www/themes')){
+            fs.readdirSync('www/themes').forEach(folder => {  
+                if(fs.existsSync('www/themes/'+folder+'/base/fonts')){
+                    paths.push('/themes/'+folder+'/base/fonts');
+                }
             });
-            if(output.length){
-                config.preload['/fonts'] = files;
-                fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
-                if(fs.existsSync('www/css/bin/fonts.css')){
-                    fs.unlinkSync('www/css/bin/fonts.css');
-                }
-                if(!fs.existsSync('www/css/bin')){
-                    fs.mkdirSync('www/css/bin', {recursive:true});
-                }
-                fs.writeFileSync('www/css/bin/fonts.css', output);               
-                if(fs.existsSync('www/'+options.index)){
-                    let index = fs.readFileSync('www/'+options.index, 'utf8');
-                    if(index.indexOf('<link href="css/bin/fonts.css" rel="stylesheet" type="text/css">') === -1){
-                        index = index.replace('</head>', '<link href="css/bin/fonts.css" rel="stylesheet" type="text/css">\n</head>');
-                        fs.writeFileSync('www/'+options.index, index);
-                    }
+        }
+        for(let i = 0; i < paths.length; i++){           
+            if(fs.existsSync('www'+paths[i])){       
+                files = [];  
+                fs.readdirSync('www'+paths[i]).forEach(folder => {  
+                    fs.readdirSync('www'+paths[i]+'/'+folder).forEach(file => {
+                        if(reg.test(file)){
+                            font = file.split('.')[0].split('=');
+                            output += '@font-face {\n';
+                            output += '    font-family:'+font[0]+';\n';
+                            output += '    src:url("../../fonts/'+folder+'/'+file+'");\n';
+                            output += '}\n';
+                            files.push(file);
+                            console.log('Info: added to preload', file);
+                            reg.lastIndex = 0;
+                        }
+                    });            
+                });
+                if(files.length){
+                    if(config.hasOwnProperty('preload')){
+                        if(config.preload.hasOwnProperty(paths[i])){
+                            delete config.preload[paths[i]];
+                        }                  
+                    } 
+                    config.preload[paths[i]] = files;
                 }
             }
-        }else{
-            console.log('Warning: folder does not exist', 'www/css');
         }
+        if(output.length){            
+            fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
+            if(fs.existsSync('www/css/bin/fonts.css')){
+                fs.unlinkSync('www/css/bin/fonts.css');
+            }
+            if(!fs.existsSync('www/css/bin')){
+                fs.mkdirSync('www/css/bin', {recursive:true});
+            }
+            fs.writeFileSync('www/css/bin/fonts.css', output);               
+            if(fs.existsSync('www/'+options.index)){
+                let index = fs.readFileSync('www/'+options.index, 'utf8');
+                if(index.indexOf('<link href="css/bin/fonts.css" rel="stylesheet" type="text/css">') === -1){
+                    index = index.replace('</head>', '<link href="css/bin/fonts.css" rel="stylesheet" type="text/css">\n</head>');
+                    fs.writeFileSync('www/'+options.index, index);
+                }
+            }            
+        }     
     }else{
         console.log('Warning: app.json file not found, preload skipped');
     }
--- cmds/images.js
+++ cmds/images.js
@@ -1,35 +1,53 @@
 #!/usr/bin/env node
 
+/*
+Should do themes as well
+*/
 module.exports = (args) => {
     const fs = require('fs');
-    const path = 'www/img';
+    const paths = ['/img'];
     let files = [];
     const reg = /(\.jpg|\.png|\.gif|\.svg)$/gi;
-    let config;
-    if(fs.existsSync(path)){
-        config = fs.readFileSync('www/app.json', 'utf-8');
-        if(config){
-            config = JSON.parse(config);
-            if(config.hasOwnProperty('preload')){
-               if(config.preload.hasOwnProperty('/img')){
-                   delete config.preload['/img'];
-               }                  
-            } 
-            fs.readdirSync(path).forEach(file => {
-                if(reg.test(file)){
-                    console.log('Info: added to preload', file);
-                    files.push(file);
+    let config;    
+    config = fs.readFileSync('www/app.json', 'utf-8');
+    if(config){
+        config = JSON.parse(config);    
+        //merge with themes
+        if(fs.existsSync('www/themes')){
+            fs.readdirSync('www/themes').forEach(folder => {  
+                if(fs.existsSync('www/themes/'+folder+'/base/img')){
+                    paths.push('/themes/'+folder+'/base/img');
                 }
-                reg.lastIndex = 0;
+                if(fs.existsSync('www/themes/'+folder+'/light/img')){
+                    paths.push('/themes/'+folder+'/light/img');
+                }
+                if(fs.existsSync('www/themes/'+folder+'/dark/img')){
+                    paths.push('/themes/'+folder+'/dark/img');
+                }
             });
-            if(files.length){
-                config.preload['/img'] = files;
-            }
-            fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
-        }else{
-            console.log('Warning: app.json file not found, preload skipped');
         }
+        for(let i = 0; i < paths.length; i++){
+            files = [];
+            if(fs.existsSync('www'+paths[i])){
+                fs.readdirSync('www'+paths[i]).forEach(file => {
+                    if(reg.test(file)){
+                        console.log('Info: added to preload', file);
+                        files.push(file);
+                    }
+                    reg.lastIndex = 0;
+                });                
+            }
+            if(files.length){
+                if(config.hasOwnProperty('preload')){
+                    if(config.preload.hasOwnProperty(paths[i])){
+                        delete config.preload[paths[i]];
+                    }                  
+                } 
+                config.preload[paths[i]] = files;
+            }
+        }        
+        fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
     }else{
-        console.log('Warning: folder does not exist', 'www/img');
-    }
+        console.log('Warning: app.json file not found, preload skipped');
+    }    
 };
