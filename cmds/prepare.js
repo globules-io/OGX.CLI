110ef1e7aaa25941ad23212625bbd3ee9c1f35f4
Eric <info@ericparlier.com>
2020-05-12 15:38:29 -0400

    added dev/prod to prepare

--- cmds/prepare.js
+++ cmds/prepare.js
@@ -2,6 +2,13 @@
 
 module.exports = (args) => {  
     console.log('Info: preparing...');
+    let options = args[args.length-1];
+    let env;
+    if(options.hasOwnProperty('env')){
+        env = options.env;
+    }else{
+        env = 'dev';
+    }
     require('./unlink.js')(['all'].concat(args));
     require('./fonts.js')(args);
     require('./images.js')(args);
@@ -11,5 +18,6 @@ module.exports = (args) => {
     require('./sounds.js')(args);
     require('./templates.js')(args);
     require('./link.js')(['all'].concat(args));
+    require('./'+env+'.js')([options]);
     console.log('Info: prepare done!');
 };
