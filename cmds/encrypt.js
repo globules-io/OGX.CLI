b2f7e9e7131f9bbd29357ee097e187407ffa8ee6
Eric <info@ericparlier.com>
2021-02-06 12:08:29 -0500

    Switch encryption lib

--- cmds/encrypt.js
+++ cmds/encrypt.js
@@ -5,12 +5,12 @@ module.exports = (args) => {
         console.log('Error: Missing arguments! Expected [key]');
         return;
     }
-    const key = args[0];
-    const aes256 = require('aes256');
+    const key = args[0];      
     const fs = require('fs');
     let conf = fs.readFileSync('www/app.json', 'utf-8');
     if(conf){
-        conf = aes256.encrypt(key, conf);
+        const CryptoJS = require('crypto-js');
+        conf = CryptoJS.AES.encrypt(conf, key, {mode:CryptoJS.mode.CBC, padding:CryptoJS.pad.Pkcs7}).toString();
         fs.writeFileSync('www/app.json', conf);   
         console.log('Info: app.json has been encrypted'); 
     }
