111f5de6ca7757fae87e54adf0723f6d0a6a8cd0
Eric <info@ericparlier.com>
2021-05-04 14:58:40 -0400

    Fix issues with install/update with package with no dependencies

--- cmds/update.js
+++ cmds/update.js
@@ -11,13 +11,21 @@ module.exports = (args) => {
     var version = false;
     o = cmd.runSync('npm show @globules-io/ogx.js version');
     latest = o.data.split('\n').shift();
-    if(pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
+    if(pkg && pkg.hasOwnProperty('dependendies') && pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
         version = pkg.dependencies['@globules-io/ogx.js'].substr(1);        
-    }
-    if(args[0] === '--force' || version && cv(latest, version) === 1){  
-        if(!pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
-            console.log('Info: installing OGX.JS '+latest);
-            cmd.runSync('npm uninstall @globules-io/ogx.js@');
+    }    
+    if(args[0] === '--force' || (version && cv(latest, version) === 1) || !version){  
+        let install = true;
+        if(pkg && pkg.hasOwnProperty('dependendies')){
+            if(pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
+                install = false;
+                console.log('Info: updating OGX.JS '+latest);
+                cmd.runSync('npm uninstall @globules-io/ogx.js@');
+                cmd.runSync('npm install @globules-io/ogx.js@'+latest);
+            }
+        }
+        if(install){
+            console.log('Info: installing @globules-io/ogx.js@'+latest);
             cmd.runSync('npm install @globules-io/ogx.js@'+latest);
         }
     }else{
