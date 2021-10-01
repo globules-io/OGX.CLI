1be3a7b286b5d9d0806e9238bf9f6b53ba6e2926
Eric <info@ericparlier.com>
2021-10-01 18:26:37 -0400

    Refactor create & delete

--- cmds/create.js
+++ cmds/create.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-
-module.exports = (args) => {
+module.exports = (args) => {    
     if(!args || args.length < 2){
         console.log('Error: Missing arguments! Expected [type] [id]');
         return;
@@ -9,69 +8,45 @@ module.exports = (args) => {
         console.log('Error: Invalid object!');
         return;
     }
-    const fs = require('fs');
-    const filename = args[0]+'.ogx';   
-    let rp =  require('path').resolve(__dirname, '../files/'+filename);
-    let file = fs.readFileSync(rp, 'utf-8');
-    file = file.replace(/{{NAME}}/gi, args[1]);
+    const fs = require('fs');    
     let links = [];
-    let template = false;
-    let config, name, dest, path;        
+    let tem;
+  
     switch(args[0]){
         case 'template':
-        path = 'www/html';   
-        name = 'template.'+args[1]+'.html'; 
-        dest = path+'/'+name;      
-        template = {path:'/html', file:name};
+        createTemplate(args[1]);        
         break;
 
         case 'view':
-        path = 'www/js/views';   
-        name = 'view.'+args[1]+'.js';    
-        dest = path+'/'+name;        
-        if(!fs.existsSync('www/css/views')){
-            fs.mkdirSync('www/css/views', {recursive:true});            
+        tem = false;
+        if(typeof(args[2]) === 'string'){
+            tem = args[2];
         }
-        fs.writeFileSync('www/css/views/view.'+args[1]+'.css', '/* CSS */');
-        links.push('<script type="application/javascript" src="js/views/'+name+'"></script>');
-        links.push('<link rel="stylesheet" href="css/views/view.'+args[1]+'.css">');
+        createView(args[1], tem);        
         break;
 
         case 'stage':
-        path = 'www/js/stages'; 
-        name = 'stage.'+args[1]+'.js';           
-        dest = path+'/'+name;    
-        if(!fs.existsSync('www/css/stages')){
-            fs.mkdirSync('www/css/stages', {recursive:true});            
+        tem = false;
+        if(typeof(args[2]) === 'string'){
+            tem = args[2];
         }
-        fs.writeFileSync('www/css/stages/stage.'+args[1]+'.css', '/* CSS */');
-        links.push('<script type="application/javascript" src="js/stages/'+name+'"></script>');
-        links.push('<link rel="stylesheet" href="css/stages/stage.'+args[1]+'.css">');
+        createStage(args[1], tem); 
         break;
 
         case 'controller':
-        path = 'www/js/controllers';   
-        name = 'controller.'+args[1]+'.js';    
-        dest = path+'/'+name;   
-        links.push('<script type="application/javascript" src="js/controllers/'+name+'"></script>');
+        createController(args[1]);
         break;        
-    }
-    if(!fs.existsSync(path)){
-        fs.mkdirSync(path, {recursive:true});
-    }
-    if(!fs.existsSync(dest)){
-        fs.writeFileSync(dest, file);    
-    }else{
-        console.log('Error: File already exists!', dest);
-        return;
-    } 
-    console.log('Created file:', args[0], dest);   
+    }   
+
     if(links.length){
-        let options = args[args.length-1];
-        file = fs.readFileSync('www/'+options.index, 'utf-8');
+        writeLinks(args);
+    }
+
+    function writeLinks(__args){
+        let options = __args[__args.length-1];
+        let file = fs.readFileSync('www/'+options.index, 'utf-8');
         if(file){
-            //add in head
-            var reg;
+            let reg;
             for(let i = 0; i < links.length; i++){
                 reg = new RegExp('/'+links[i]+'/gi');
                 if(!reg.test(file)){
@@ -85,28 +60,134 @@ module.exports = (args) => {
         }else{
             console.log('Warning:', options.index, 'file not found, linking skipped');
         }
-    }else{
-        if(template){
-            config = fs.readFileSync('www/app.json', 'utf-8');
+    } 
+
+    function createController(__name){
+        let file = 'controller.'+__name+'.js';
+        let made = makeFile('controller', __name);
+        if(made){
+            links.push('<script type="application/javascript" src="js/controllers/'+file+'"></script>');
+        }      
+    }
+    
+    function createView(__name, __template_name){
+        let file = 'view.'+__name+'.js';       
+        let made = makeFile('view', __name);
+        if(made){  
+            if(!fs.existsSync('www/css/views')){
+                fs.mkdirSync('www/css/views', {recursive:true});            
+            }
+            fs.writeFileSync('www/css/views/view.'+__name+'.css', '/* CSS */');     
+            links.push('<link rel="stylesheet" href="css/views/view.'+__name+'.css">');    
+            links.push('<script type="application/javascript" src="js/views/'+file+'"></script>');
+            if(__template_name){
+                createTemplate(__template_name);
+            }
+        }              
+    }
+
+    function createStage(__stage_name, __template_name){         
+        let made = makeFile('stage', __stage_name);
+        if(made){              
+            if(!fs.existsSync('www/css/stages')){
+                fs.mkdirSync('www/css/stages', {recursive:true});            
+            }
+            fs.writeFileSync('www/css/stages/stage.'+__stage_name+'.css', '/* CSS */');
+            links.push('<script type="application/javascript" src="js/stages/stage.'+__stage_name+'.js"></script>');
+            links.push('<link rel="stylesheet" href="css/stages/stage.'+__stage_name+'.css">');
+            config = loadConfig();
             if(config){
-                config = JSON.parse(config);
-                if(!config){
-                    console.log('Error: app.json decoding error');
-                    return;
+                if(!config.hasOwnProperty('vapps')){
+                    config.vapps = {};
                 }
-                if(!config.hasOwnProperty('preload')){
-                    config.preload = {};
-                }                
-                if(!config.preload.hasOwnProperty(template.path)){
-                    config.preload[template.path] = [template.file];                    
-                }else{
-                    config.preload[template.path].push(template.file);
+                let t = '';
+                if(__template_name){
+                    saveConfig(config);
+                    createTemplate(__template_name);
+                    t = __template_name;   
+                    config = loadConfig();
                 }
-                console.log('Info: Added to preload', template);  
-                fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
+                config.vapps[args[1].toLowerCase()+':Stages.'+args[1]] = {
+                    template:t,
+                    use:false,
+                    home:'',
+                    scope:['public'],
+                    theater:false
+                }
+                saveConfig(config);
+            }else{           
+                console.log('Warning: app.json file not found');      
+            }  
+        }      
+    }
+
+    function createTemplate(__name){
+        let path = '/html';  
+        let file = 'template.'+__name+'.html'; 
+        fs.writeFileSync('www/html/'+file, '<span></span>'); 
+        let config = loadConfig();
+        if(config){   
+            if(!config.hasOwnProperty('preload')){
+                config.preload = {};
+            }                
+            if(!config.preload.hasOwnProperty(path)){
+                config.preload[path] = [file];                    
             }else{
-                console.log('Warning: app.json file not found, preload skipped');
-            }           
+                config.preload[path].push(file);
+            }
+            console.log('Info: Added to preload', path+'/'+file); 
+            saveConfig(config);
+        }else{
+            console.log('Warning: app.json file not found, preload skipped');
+        } 
+    }
+
+    function makeFile(__type, __name){
+        const filename = __type+'.ogx';       
+        let rp =  require('path').resolve(__dirname, '../files/'+filename);
+        let file = fs.readFileSync(rp, 'utf-8');
+        file = file.replace(/{{NAME}}/gi, __name);
+        let path, name, dest;
+        switch(__type){
+            case 'view':
+            path = 'www/js/views';
+            name = 'view.'+__name+'.js';    
+            break;           
+
+            case 'controller':
+            path = 'www/js/controllers';
+            name = 'controller.'+__name+'.js';    
+            break;
+
+            case 'stage':
+            path = 'www/js/stages';
+            name = 'stage.'+__name+'.js';    
+            break;
         }
-    }    
-}
+        dest = path+'/'+name;
+        if(!fs.existsSync(path)){
+            fs.mkdirSync(path, {recursive:true});
+        }
+        if(!fs.existsSync(dest)){
+            fs.writeFileSync(dest, file);    
+        }else{
+            console.log('Error: File already exists!', dest);
+            return false;
+        } 
+        return true;
+    }
+
+    function loadConfig(){
+        let config = fs.readFileSync('www/app.json', 'utf-8');
+        if(config){
+            config = JSON.parse(config);
+            return config;
+        }       
+        console.log('Error: app.json decoding error');   
+        return false;
+    };
+
+    function saveConfig(__config){
+        fs.writeFileSync('www/app.json', JSON.stringify(__config, null, 4));
+    };
+};
--- cmds/delete.js
+++ cmds/delete.js
@@ -10,94 +10,137 @@ module.exports = (args) => {
         return;
     }
     const fs = require('fs');
-    let links = [];
-    let template = false;
-    let config, dest, path, name, file;       
+    let links = [];  
     switch(args[0]){
         case 'template':
-        path = 'www/html';    
-        name = 'template.'+args[1]+'.html';  
-        dest = path+'/'+name;      
-        template = {path:'/html', file:'template.'+args[1]+'.html'};
+        deleteTemplate(args[1]);        
         break;
 
         case 'view':
-        path = 'www/js/views';      
-        name = 'view.'+args[1]+'.js';
-        dest = path+'/'+name;
-        links.push('<script type="application/javascript" src="js/views/'+name+'"></script>');
-        if(fs.existsSync('www/css/views/view.'+args[1]+'.css')){
-            fs.unlinkSync('www/css/views/view.'+args[1]+'.css');
-        }
-        links.push('<link rel="stylesheet" href="css/views/view.'+args[1]+'.css">');
+        deleteView(args[1]);       
         break;
 
         case 'controller':
-        path = 'www/js/controllers';   
-        name = 'controller.'+args[1]+'.js';
-        dest = path+'/'+name;
-        links.push('<script type="application/javascript" src="js/controllers/'+name+'"></script>');
+        deleteController(args[1]);       
         break;
 
         case 'stage':
-        path = 'www/js/stages';      
-        name = 'stage.'+args[1]+'.js';  
-        dest = path+'/'+name;
-        links.push('<script type="application/javascript" src="js/stages/'+name+'"></script>');
-        if(fs.existsSync('www/css/stages/stage.'+args[1]+'.css')){
-            fs.unlinkSync('www/css/stages/stage.'+args[1]+'.css');
-        }
-        links.push('<link rel="stylesheet" href="css/stages/stage.'+args[1]+'.css">');
+        deleteStage(args[1]);       
         break;
     } 
-    if(!fs.existsSync(dest)){
-        console.log('Warning: File already deleted!', dest);
-    }else{
-        fs.unlinkSync(dest);
-        console.log('Info: Deleted file', dest);
-    }
+
     if(links.length){
-        let options = args[args.length-1];
-        file = fs.readFileSync('www/'+options.index, 'utf-8');
+       removeLinks(args);
+    }
+
+    function deleteTemplate(__name){
+        let filename = 'template.'+__name+'.html';  
+        let filepath = '/www/html/'+filename;       
+        let config = loadConfig();
+        if(config){
+            let idx = config.preload['/html'].indexOf(filename);
+            if(idx !== -1){
+                config.preload['/html'].splice(idx, 1);
+                saveConfig(config);
+                console.log('Info: File removed from preload', filename);
+            }else{
+                console.log('Warning: File not found in preload!', filename);
+            }
+        }else{
+            console.log('Warning: app.json file not found');      
+        }
+        if(!fs.existsSync(filepath)){
+            console.log('Warning: File already deleted!', filename);
+        }else{
+            fs.unlinkSync(filepath);
+            console.log('Info: Deleted file', filename);
+        }
+    }
+
+    function deleteStage(__name){
+        let filename = 'stage.'+__name+'.js';  
+        let filepath = 'www/js/stages/'+filename;    
+        if(!fs.existsSync(filepath)){
+            console.log('Warning: File already deleted!', filename);
+        }else{
+            fs.unlinkSync(filepath);
+            links.push('<script type="application/javascript" src="js/stages/'+filename+'"></script>')
+            console.log('Info: Deleted file', filename);
+        }
+        let config = loadConfig();
+        if(config){
+            let vapp = __name.toLowerCase()+'.Stages.'+__name;
+            for(let a in config.vapps){
+                if(a === vapp){
+                    delete config.vapps[a];
+                    console.log('Info: Deleted virtual app', __name);
+                    break;
+                }
+            }
+            saveConfig(config);
+        }else{
+            console.log('Warning: app.json file not found');      
+        }
+    }
+
+    function deleteController(__name){
+        let filename = 'controller.'+__name+'.js';  
+        let filepath = 'www/js/controllers/'+filename;   
+         if(!fs.existsSync(filepath)){
+            console.log('Warning: File already deleted!', filename);
+        }else{
+            fs.unlinkSync(filepath);
+            links.push('<script type="application/javascript" src="js/controllers/'+filename+'"></script>');
+            console.log('Info: Deleted file', filename);
+        }         
+    }
+
+    function deleteView(__name){
+        let filename = 'view.'+__name+'.js';
+        let filepath = 'www/js/views/'+filename; 
+        if(fs.existsSync(filepath)){
+            fs.unlinkSync(filepath);
+            links.push('<script type="application/javascript" src="js/views/'+filename+'"></script>');
+            console.log('Info: Deleted file', filename);
+        }  
+        filename = 'view.'+__name+'.css';
+        filepath = 'www/css/views/'+filename;
+        if(fs.existsSync(filepath)){
+            fs.unlinkSync(filepath);
+            links.push('<link rel="stylesheet" href="css/views/view.'+__name+'.css">');
+            console.log('Info: Deleted file', filename);
+        }else{
+            console.log('Warning: File already deleted!', filename);
+        }
+    }
+
+    function removeLinks(__args){
+        let options = __args[__args.length-1];
+        let file = fs.readFileSync('www/'+options.index, 'utf-8');
         if(file){
             for(let i = 0; i < links.length; i++){
                 if(file.indexOf(links[i]) !== -1){
                     file = file.replace(links[i], '');                    
                     console.log('Unlinked File:', links[i]);  
                 }else{
-                    console.log('Warning: File already unlinked!', dest);
+                    console.log('Warning: File already unlinked!', links[i]);
                 }
             }
             fs.writeFileSync('www/'+options.index, file);
         }
-    }else{
-        if(template){
-            config = fs.readFileSync('www/app.json', 'utf-8');
-            if(config){
-                config = JSON.parse(config);
-                if(!config){
-                    console.log('Error: app.json decoding error');
-                    return;
-                }
-                if(config.hasOwnProperty('preload')){                    
-                    let idx;
-                    if(config.preload.hasOwnProperty(template.path)){
-                        idx = config.preload[template.path].indexOf(template.file);
-                        if(idx !== -1){
-                            config.preload[template.path].splice(idx, 1);
-                            if(!config.preload[template.path].length){
-                                delete config.preload[template.path];
-                            }
-                            fs.writeFileSync('www/app.json', JSON.stringify(config, null, 4));
-                            console.log('Info: Removed from preload', template);  
-                        }else{
-                            console.log('Warning: File not found in preload, skipped', template);  
-                        }
-                    }
-                }
-            }else{
-                console.log('Warning: app.json file not found, preload skipped');
-            }
-        }   
     }
+    
+    function loadConfig(){
+        let config = fs.readFileSync('www/app.json', 'utf-8');
+        if(config){
+            config = JSON.parse(config);
+            return config;
+        }       
+        console.log('Error: app.json decoding error');   
+        return false;
+    };
+
+    function saveConfig(__config){
+        fs.writeFileSync('www/app.json', JSON.stringify(__config, null, 4));
+    };
 };
