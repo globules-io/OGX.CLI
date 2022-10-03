fcc6b90d012b6a77d6c4bdef8a130ed7f5719417
Eric <info@ericparlier.com>
2022-10-03 09:30:37 -0400

    Update use DEV from github with --dev

--- cmds/update.js
+++ cmds/update.js
@@ -4,7 +4,6 @@ module.exports = (args) => {
     
     const cv = require('compare-versions');
     const cmd = require('node-cmd');
-    const fs = require('fs');
     const cmd_dir = process.cwd();   
 
     let pkg = require(cmd_dir+'/package.json');
@@ -13,9 +12,33 @@ module.exports = (args) => {
     latest = o.data.split('\n').shift();
     if(pkg && pkg.hasOwnProperty('dependendies') && pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
         version = pkg.dependencies['@globules-io/ogx.js'].substr(1);        
-    }    
-    if(args[0] === '--force' || (version && cv(latest, version) === 1) || !version){  
-        let install = true;
+    }   
+
+    let install;
+
+    //need --dev to down the latest dev build
+    if(args.includes('--dev')){
+        install = true;
+        if(pkg && pkg.hasOwnProperty('dependendies')){
+            if(pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
+                install = false;
+                console.log('Info: installing OGX.JS DEV from Github');
+                cmd.runSync('npm uninstall @globules-io/ogx.js');
+                cmd.runSync('npm install https://github.com/globules-io/OGX.JS');
+                console.log('Info: OGX.JS DEV from Github installed');                
+            }
+        }
+        if(install){
+            console.log('Info: installing OGX.JS DEV from Github');
+            cmd.runSync('npm install @globules-io/ogx.js@'+latest);
+            cmd.runSync('npm install https://github.com/globules-io/OGX.JS');
+            console.log('Info: OGX.JS DEV from Github installed');   
+        }
+        return;
+    }
+
+    if(args.includes('--force') || (version && cv(latest, version) === 1) || !version){  
+        install = true;
         if(pkg && pkg.hasOwnProperty('dependendies')){
             if(pkg.dependencies.hasOwnProperty('@globules-io/ogx.js')){
                 install = false;
