a2f53dd1869fce3c6865173a3adb120cd44f134d
Eric <info@ericparlier.com>
2024-01-06 11:40:50 +0100

    Open cmd open file type and name

--- cmds/open.js
+++ cmds/open.js
@@ -1,49 +1,87 @@
 #!/usr/bin/env node
-/*
-D:\Server\ROOT\KRUBOSS_PWA\www\css\bin>code -r icons.css
-*/
 module.exports = (args) => {    
     if(!args || args.length < 1){
-        console.log('Error: Missing arguments! Expected [name]');
+        console.log('Error: Missing arguments! Expected [name] or [type] [name]');
         return;
     }
     console.log('Info: Opening files');
     const exec = require('child_process').execSync;
     const fs = require('fs');
-    const name = args[0];
+
+    let name;
+    let type = null;
+    let lookup_all = args.length === 1;
+    lookup_all ? name = args[0] : [name, type] = [args[0], args[1]];
+
+    if(lookup_all || type === 'js'){
+        lookupViewJS(name);
+    }
+
+    if(lookup_all || type === 'css'){
+        lookupViewCSS(name);
+    }
+
+    if(lookup_all || type === 'html'){
+        lookupViewHTML(name);
+    }  
+
+    if(lookup_all || type === 'oml'){
+        lookupViewOML(name);
+    }   
+
+    if(type === 'json'){
+        lookupJSON(__name);
+    }
 
     //lookup view js
-    fs.readdirSync('www/js/views').forEach(file => {
-        if(file.toLowerCase() === 'view.'+name.toLowerCase()+'.js'){
-            console.log('Info: Found css view', file);
-            openFile('www/js/views', file);
-        }
-    });
+    function lookupViewJS(__name){
+        fs.readdirSync('www/js/views').forEach(file => {
+            if(file.toLowerCase() === 'view.'+__name.toLowerCase()+'.js'){
+                console.log('Info: Found css view', file);
+                openFile('www/js/views', file);
+            }
+        });
+    }
 
     //lookup view css
-    fs.readdirSync('www/css/views').forEach(file => {
-        if(file.toLowerCase() === 'view.'+name.toLowerCase()+'.css'){
-            console.log('Info: Found js view', file);
-            openFile('www/css/views', file);
-        }
-    });
+    function lookupViewCSS(__name){
+        fs.readdirSync('www/css/views').forEach(file => {
+            if(file.toLowerCase() === 'view.'+__name.toLowerCase()+'.css'){
+                console.log('Info: Found js view', file);
+                openFile('www/css/views', file);
+            }
+        });
+    }
 
     //lookup template
-    fs.readdirSync('www/html').forEach(file => {
-        if(file.toLowerCase() === 'template.'+name.toLowerCase()+'.html'){
-            console.log('Info: Found template', file);
-            openFile('www/html', file);
-        }
-    });
+    function lookupViewHTML(__name){
+        fs.readdirSync('www/html').forEach(file => {
+            if(file.toLowerCase() === 'template.'+__name.toLowerCase()+'.html'){
+                console.log('Info: Found template', file);
+                openFile('www/html', file);
+            }
+        });
+    }
 
     //lookup oml
-    fs.readdirSync('www/oml').forEach(file => {
-        if(file.toLowerCase() === name.toLowerCase()+'.oml'){
-            console.log('Info: Found oml', file);
-            openFile('www/oml', file);
-        }
-    });
-    
+    function lookupViewOML(__name){
+        fs.readdirSync('www/oml').forEach(file => {
+            if(file.toLowerCase() === __name.toLowerCase()+'.oml'){
+                console.log('Info: Found oml', file);
+                openFile('www/oml', file);
+            }
+        });
+    }    
+
+    //lookup json
+    function lookupJSON(__name){
+        fs.readdirSync('www/json').forEach(file => {
+            if(file.toLowerCase() === __name.toLowerCase()+'.json'){
+                console.log('Info: Found json', file);
+                openFile('www/json', file);
+            }
+        });
+    }  
 
     function openFile(__path, __file){
         let com = 'code -r '+__file;
