2c59e1095fdf93ad1acfe5df4419408ba0abc7bb
Eric <info@ericparlier.com>
2024-03-31 17:28:42 +0200

    Fix link unlink

--- cmds/link.js
+++ cmds/link.js
@@ -14,56 +14,56 @@ module.exports = (args) => {
         const fs = require('fs');
         let index = fs.readFileSync('www/'+index_file, 'utf-8');
         if(index){
-            if(['all', 'controllers'].includes(args[0])){               
+            if(['all', 'controllers'].includes(args[0])){   
                 fs.readdirSync('www/js/controllers').forEach(file => {  
                     if(/\.js$/.test(file)){
-                        if(!index.includes('<script type="application/javascript" src="js/controllers/'+file+'">')){                
+                        if(!index.includes('js/controllers/'+file)){                
                             index = index.replace('</head>', '<script type="application/javascript" src="js/controllers/'+file+'"></script>\n</head>');  
                         }
                         console.log('Info: linked', file);
                     }
                 });
             }  
-            if(['all', 'views'].includes(args[0])){               
+            if(['all', 'views'].includes(args[0])){  
                 fs.readdirSync('www/js/views').forEach(file => {   
                     if(/\.js$/.test(file)){   
-                        if(!index.includes('<script type="application/javascript" src="js/views/'+file+'">')){                            
+                        if(!index.includes('js/views/'+file)){                            
                             index = index.replace('</head>', '<script type="application/javascript" src="js/views/'+file+'"></script>\n</head>');  
                         }
-                        if(!index.includes('<link rel="stylesheet" href="css/views/view.'+file.split('.')[1]+'.css">')){     
-                            index = index.replace('</head>', '<link rel="stylesheet" href="css/views/view.'+file.split('.')[1]+'.css">\n</head>'); 
+                        if(!index.includes('css/views/view.'+file.split('.')[1]+'.css')){     
+                            index = index.replace('</head>', '<link rel="stylesheet" href="css/views/view.'+file.split('.')[1]+'.css" type="text/css">\n</head>'); 
                         }
                         console.log('Info: linked', file);
                     }
                 });
             }             
-            if(['all', 'stages'].includes(args[0])){   
+            if(['all', 'stages'].includes(args[0])){    
                 fs.readdirSync('www/js/stages').forEach(file => {   
                     if(/\.js$/.test(file)){
-                        if(!index.includes('<script type="application/javascript" src="js/stages/'+file+'">')){                              
+                        if(!index.includes('js/stages/'+file)){                              
                             index = index.replace('</head>', '<script type="application/javascript" src="js/stages/'+file+'"></script>\n</head>');  
                         }
-                        if(!index.includes('<link rel="stylesheet" href="css/stages/stage.'+file.split('.')[1]+'.css">')){            
-                            index = index.replace('</head>', '<link rel="stylesheet" href="css/stages/stage.'+file.split('.')[1]+'.css">\n</head>'); 
+                        if(!index.includes('stage.'+file.split('.')[1]+'.css')){            
+                            index = index.replace('</head>', '<link rel="stylesheet" href="css/stages/stage.'+file.split('.')[1]+'.css" type="text/css">\n</head>'); 
                         }
                         console.log('Info: linked', file);
                     }
                 });       
             } 
-            if(['all', 'css'].includes(args[0])){   
+            if(['all', 'css'].includes(args[0])){                   
                 fs.readdirSync('www/css/bin').forEach(file => {   
                     if(/\.css$/.test(file)){
-                        if(!index.includes('<link rel="stylesheet" href="css/bin/'+file+'">')){                           
-                            index = index.replace('</head>', '<link rel="stylesheet" href="css/bin/'+file+'">\n</head>');  
+                        if(!index.includes('css/bin/'+file)){                           
+                            index = index.replace('</head>', '<link rel="stylesheet" href="css/bin/'+file+'" type="text/css">\n</head>');  
                         }
                         console.log('Info: linked', file);
                     }
                 });       
             }
-            if(['all', 'js'].includes(args[0])){   
+            if(['all', 'js'].includes(args[0])){                 
                 fs.readdirSync('www/js/bin').forEach(file => {   
                     if(/\.js$/.test(file)){
-                        if(!index.includes('<script type="application/javascript" src="js/bin/'+file+'">')){                              
+                        if(!index.includes('js/bin/'+file)){                              
                             index = index.replace('</head>', '<script type="application/javascript" src="js/bin/'+file+'"></script>\n</head>');  
                         }
                         console.log('Info: linked', file);
--- cmds/unlink.js
+++ cmds/unlink.js
@@ -14,62 +14,33 @@ module.exports = (args) => {
         let options = args[args.length-1];
         let index = fs.readFileSync('www/'+options.index, 'utf-8');
         if(index){
-            if(['all', 'controllers'].includes(args[0])){               
-                fs.readdirSync('www/js/controllers').forEach(file => {      
-                    if(/\.js$/.test(file)){            
-                        index = index.replace('<script type="application/javascript" src="js/controllers/'+file+'"></script>\n', '');  
-                        console.log('Info: unlinked', file);
-                    }
-                });
+            if(['all', 'controllers'].includes(args[0])){       
+                index = index.replace(/<script (.*)js\/controllers\/(.+)><\/script>(\r\n|\r|\n)*/gim, '');  
+                console.log('Info: unlinked js/controllers');  
             }   
-            if(['all', 'views'].includes(args[0])){               
-                fs.readdirSync('www/js/views').forEach(file => {    
-                    if(/\.js$/.test(file)){              
-                        index = index.replace('<script type="application/javascript" src="js/views/'+file+'"></script>\n', '');  
-                        index = index.replace('<link rel="stylesheet" href="css/views/view.'+file.split('.')[1]+'.css">\n', ''); 
-                        console.log('Info: unlinked', file);
-                    }
-                });
+            if(['all', 'views'].includes(args[0])){  
+                index = index.replace(/<script (.*)js\/views\/(.+)><\/script>(\r\n|\r|\n)*/gim, '');
+                index = index.replace(/<link (.*)css\/views\/(.+)>(\r\n|\r|\n)*/gim, '');
+                console.log('Info: unlinked js/views');       
+                console.log('Info: unlinked css/views');  
             }            
             if(['all', 'stages'].includes(args[0])){   
-                fs.readdirSync('www/js/stages').forEach(file => {   
-                    if(/\.js$/.test(file)){               
-                        index = index.replace('<script type="application/javascript" src="js/stages/'+file+'"></script>\n', '');  
-                        index = index.replace('<link rel="stylesheet" href="css/stages/stage.'+file.split('.')[1]+'.css">\n', ''); 
-                        console.log('Info: unlinked', file);
-                    }
-                });       
+                index = index.replace(/<script (.*)js\/stages\/(.+)><\/script>(\r\n|\r|\n)*/gim, '');
+                index = index.replace(/<link (.*)css\/stages\/(.+)>(\r\n|\r|\n)*/gim, ''); 
+                console.log('Info: unlinked js/stages');       
+                console.log('Info: unlinked css/stages');  
             }   
             if(['all', 'css'].includes(args[0])){   
-                index = clearBin(index, args[0]);
+                index = index.replace(/<link (.*)css\/bin\/(.+)>(\r\n|\r|\n)*/gim, ''); 
                 console.log('Info: unlinked css/bin');                
             }  
             if(['all', 'js'].includes(args[0])){   
-                index = clearBin(index, args[0]);
+                index = index.replace(/<script (.*)js\/bin\/(.+)><\/script>(\r\n|\r|\n)*/gim, '');
                 console.log('Info: unlinked js/bin');                   
             }  
             fs.writeFileSync('www/'+options.index, index);    
         }else{
             console.log('Warning: index.html file not found, unlink skipped');
         } 
-    }
-
-    function clearBin(__index, __type){
-        let reg;        
-        switch(__type){
-            case 'all':
-            reg = /(<link rel="stylesheet" href="css|<script type="application\/javascript" src="js)\/bin\/[a-zA-Z0-9\.\/_\-"= ]+>(<\/script>)?(\r\n|\n|\r)?/gi;
-            break;
-
-            case 'css':
-            reg = /<link rel="stylesheet" href="css\/bin\/[a-zA-Z0-9\.\/_\-"= ]+>(\r\n|\n|\r)?/gi;
-            break;
-
-            case 'js':
-            reg = /<script type="application\/javascript" src="js\/bin\/[a-zA-Z0-9\.\/_\-"= ]+><\/script>(\r\n|\n|\r)?/gi;
-            break;
-        }
-        __index = __index.replaceAll(reg, '');       
-        return __index;
-    }
+    }  
 };
